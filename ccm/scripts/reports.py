97a1f72c20a57b4445719d849ab0067e91adda18
Dustin J. Mitchell <dustin@mozilla.com>
2013-11-21 13:40:53 -0500

    add CSV output

--- ccm/scripts/reports.py
+++ ccm/scripts/reports.py
@@ -1,4 +1,7 @@
 import time
+import sys
+import csv
+import itertools
 import ccm.vcs
 import ccm.scripts.base
 
@@ -7,11 +10,15 @@ def main():
         parser.add_argument('--days', metavar='DAYS',
                 type=int, help='number of days over which to report',
                 default=1)
+        parser.add_argument('--format', help='report format',
+                default='html', choices=('html', 'csv'))
     db, logger, parser, args, cfg = ccm.scripts.base.set_up(
             'ccm-reports',
             'Generate reports',
             extra_args)
 
+    # first, generate an actual rectangular array of data, along with
+    # row and column titles
     grid = db.changes_by_user_repo(days_back=args.days)
     users = grid.keys()
     users.sort()
@@ -19,12 +26,25 @@ def main():
         row.keys() for row in grid.values()
     ], set()))
     repos.sort()
+    grid = [ [ grid[u].get(r, (0,0)) for r in repos ] for u in users ]
 
-    def pm(tup):
-        return "<span class='plus'>+%s</span>/<span class='minus'>-%s</span>" % tup
+    if args.format == 'html':
+        output_html(grid, users, repos, args)
+    elif args.format == 'csv':
+        output_csv(grid, users, repos, args)
+    else:
+        parser.error("unknown format %s" % format)
 
+def grid_sum(cells):
     def add(tup1, tup2):
         return tup1[0]+tup2[0], tup1[1]+tup2[1]
+    return reduce(add, cells, (0,0))
+
+def output_html(grid, users, repos, args):
+    def pm(tup):
+        if tup[0] == tup[1] == 0:
+            return ''
+        return "<span class='plus'>+%s</span>/<span class='minus'>-%s</span>" % tup
 
     start_date = time.asctime(time.localtime(time.time() - args.days*3600*24))
     end_date = time.asctime(time.localtime(time.time()))
@@ -42,27 +62,26 @@ def main():
         print " <th>%s</th>" % repo
     print " <th>total</th>"
     print "</tr>"
-    total_per_repo = {}
-    for user in users:
+    for user, row in itertools.izip(users, grid):
         print "<tr><th>%s</th>" % user
-        total = (0, 0)
-        for repo in repos:
-            count = grid[user].get(repo)
-            if count:
-                total = add(total, count)
-                total_per_repo[repo] = \
-                    add(total_per_repo.get(repo, (0,0)), count)
-                count = pm(count)
-            else:
-                count = ''
-            print " <td>%s</td>" % count
-        print " <td>%s</td>" % pm(total)
+        for cell in row:
+            print " <td>%s</td>" % pm(cell)
+        print " <td>%s</td>" % pm(grid_sum(row))
         print "</tr>"
     print "<tr><th>total</th>"
-    for repo in repos:
-        print " <th>%s</th>" % pm(total_per_repo[repo])
-    print " <th>%s</th>" % pm(reduce(add, total_per_repo.values(), (0,0)))
+    for r in xrange(len(repos)):
+        print " <th>%s</th>" % pm(grid_sum(grid[u][r] for u in xrange(len(users))))
+    print " <th>%s</th>" % pm(grid_sum(grid_sum(grid[u][r] for u in xrange(len(users))) for r in xrange(len(repos))))
     print "</tr>"
     print "</table>"
     print "</body>"
     print "</html>"
+
+def output_csv(grid, users, repos, args):
+    w = csv.writer(sys.stdout)
+    def flatten(l):
+        return itertools.chain.from_iterable(l)
+    w.writerow([''] + list(flatten(('+'+r, '-'+r) for r in repos)))
+
+    for user, row in itertools.izip(users, grid):
+        w.writerow([user] + list(flatten(row)))
