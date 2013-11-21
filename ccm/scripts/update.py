0b145771169bfee29f19ef276ef6561a104e20b8
Dustin J. Mitchell <dustin@mozilla.com>
2013-11-21 15:23:50 -0500

    allow ccm-update to handle a single repository

--- ccm/scripts/update.py
+++ ccm/scripts/update.py
@@ -4,14 +4,18 @@ import ccm.scripts.base
 def main():
     def extra_args(parser):
         parser.add_argument('--days', metavar='DAYS',
-                type=int, help='number of days over which to report',
+                type=int, help='number of days back to scan',
                 default=10)
+        parser.add_argument('--repository',
+                help='repository to scan; defaults to all')
     db, logger, parser, args, cfg = ccm.scripts.base.set_up(
             'ccm-update',
             'Update version-control repositories',
             extra_args)
 
     for repo in ccm.vcs.load_all(cfg):
+        if args.repository and repo.name != args.repository:
+            continue
         logger.info('processing %s' % repo)
         repo.update()
         repoid = db.get_repoid(repo.name, repo.url)
